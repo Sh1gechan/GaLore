f8f6c2a48bacbbcfb579902a82a47646c9518a52
Sh1gechan <U.bolt2170@gmail.com>
2024-04-29 22:04:29 +0900

    SETTING.mdを追加

---
+++ tsubame.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -cwd
+#$ -l node_h=1
+#$ -l h_rt=7:00:00
+#$ -p -5
+
+export HF_HOME=/gs/fs/tge-24IJ0070/HF_HOME
+
+module load cuda/11.8
+module load cudnn/cuda-11.x/8.9.0
+
+source .env/bin/activate
+
+export MASTER_ADDR=$(/usr/sbin/ip a show | grep inet | grep 192.168.205 | head -1 | cut -d " " -f 6 | cut -d "/" -f 1)
+export MASTER_PORT=$((10000 + ($SLURM_JOBID % 50000)))
+
+torchrun --standalone --nproc_per_node 1 torchrun_main.py \
+    --model_config configs/llama_7b.json \
+    --lr 0.005 \
+    --galore_scale 0.25 \
+    --rank 1024 \
+    --update_proj_gap 500 \
+    --batch_size 16 \
+    --total_batch_size 512 \
+    --activation_checkpointing \
+    --num_training_steps 150000 \
+    --warmup_steps 15000 \
+    --weight_decay 0 \
+    --grad_clipping 1.0 \
+    --dtype bfloat16 \
+    --eval_every 1000 \
+    --single_gpu \
+    --optimizer galore_adamw8bit_per_layer
